03c67c8e6e60744ed91907f80f6e5cc0d0942d1f
Silvana <spaezeduc@gmail.com>
2024-06-14 20:56:19 -0300

    Agregando las carpetas programacion3 Diseño

---
+++ TEORIA/08-Bootstrap/ejemplo/scrip.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+// Puedes agregar scripts personalizados aquí
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Ejemplo de script: Mostrar alerta al cargar la página
+    console.log("Página cargada correctamente");
+  });
+  
